f532b4de24a9e258b750168a7be8f12fb444d3a8
Maisie Campbell <maisie-campbell@hotmail.com>
2017-06-11 23:05:07 +0100

    setting up ddata charts

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,75 +1,86 @@
 queue()
-   .defer(d3.json, "/fitbitData/May")
+   .defer(d3.json, "/fitbit/activity")
    .await(makeGraphs);
 
-function makeGraphs(error, projectsJson) {
+function makeGraphs(error, activityJson) {
 
     //Clean projectsJson data
-    var healthOverviewHealthData = projectsJson;
+    var fitbitActivity = activityJson;
     var dateFormat = d3.time.format("%Y-%m-%d");
+    fitbitActivity.forEach(function (d) {
+      d["date"] = dateFormat.parse(d["date"]);
+      d["calories_burned"] = +d["calories_burned"];
+      d["steps"] = +d["steps"];
+      d["distance"] = +d["distance"]; // unsure aboout distance km?
+      d["floors"] = +d["floors"];
+      // d["minutes_sedentary"] = +d["minutes_sedentary"];
+      d["minutes_lightly_active"] = +d["minutes_lightly_active"];
+      //d["minutes_fairly_active"] = +d["minutes_fairly_active"];
+      d["minutes_very_active"] = +d["minutes_very_active"];
+      d["activity_calories"] = +d["activity_calories"];
+
+    });
+
 
 
     //Create a Crossfilter instance
+    var ndx = crossfilter(fitbitActivity);
 
 
+    //Define data dimensions 
+    var dateDim =ndx.dimension(function(d) { 
+      return d["date"]; 
+    });
+    var caloriesDim =ndx.dimension(function(d) { 
+      return d["calories_burned"]; 
+    });
+    var stepsDim =ndx.dimension(function(d) { 
+      return d["steps"]; 
+    });
+    var distanceDim =ndx.dimension(function(d) { 
+      return d["distance"]; 
+    });
+    var floorsDim =ndx.dimension(function(d) { 
+      return d["floors"]; 
+    });
+    var minsLightlyActiveDim =ndx.dimension(function(d) {
+      return d["minutes_lightly_active"]; 
+    });
+    var minsVeryActiveDim =ndx.dimension(function(d) {
+      return d["minutes_very_active"]; 
+    });
+    var activityCaloriesDim =ndx.dimension(function(d) {
+      return d["activity_calories"]; 
+    });
 
-    //Calculate metrics
 
-
+    // Define Data Groups, Calculate Metrics
+    var numRecordsByDate = dateDim.group();
+    var caloriesGroup = caloriesDim.group();
+    var stepsGroup = stepsDim.group();
+    var distanceGroup = distanceDim.group();
+    var floorsGroup = floorsDim.group();
+    //var minsSedentaryGroup = minsSedentaryDim.group();
+    var minsLightlyActiveGroup = minsLightlyActiveDim.group();
+    //var minsFairlyActiveGroup = minsFairlyActiveDim.group();
+    var minsVeryActiveGroup = minsVeryActiveDim.group();
+    var activityCaloriesGroup = activityCaloriesDim.group();
+    var all = ndx.groupAll();
 
 
     //Define values (to be used in charts)
-    var minDate = dateDim.bottom(1)[0]["ACTIVITY DATE"];
-    var maxDate = dateDim.top(1)[0]["ACTIVITY DATE"];
+    var minDate = dateDim.bottom(1)[0]["date"];
+    var maxDate = dateDim.top(1)[0]["date"];
 
     //Charts
+    var timeChart = dc.barChart("#time-chart");
     var caloriesChart = dc.barChart("#calories-burned-chart");
     var stepsChart = dc.barChart("#steps-chart");
-    var distanceChart = dc.barChart("#distance-chart");
-    var floorsChart = dc.barChart("#floors-chart");
+    var distanceND = dc.numberDisplay("#distance-chart");
+    var floorsND = dc.numberDisplay("#floors-chart");
 
 
-
-    distanceChart
-       .width(300)
-       .height(250)
-       .dimension(distanceDim)
-       .group(numDistance)
-       .xAxis().ticks(4);
-
-    floorsChart
-       .width(300)
-       .height(250)
-       .dimension(floorsDim)
-       .group(numFloors)
-       .xAxis().ticks(4);
-
-
-    timeChart
-        .width(800)
-        .height(200)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
-        .dimension(dateDim)
-        .group(numActivityByDate)
-        .transitionDuration(500)
-        .x(d3.time.scale().domain([minDate, maxDate]))
-        .elasticY(true)
-        .xAxisLabel("Year")
-        .yAxis().ticks(4);
-
-    caloriesChart
-        .width(300)
-        .height(250)
-        .dimension(caloriesDim)
-        .group(numCalories)
-        .xAxis().ticks(4);
-
-    stepsChart
-        .width(300)
-        .height(250)
-        .dimension(stepsDim)
-        .group(numSteps)
-        .xAxis().ticks(4);
+   //Chart Parameters
 
 
     dc.renderAll();
