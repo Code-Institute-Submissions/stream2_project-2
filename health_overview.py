004e1a154aea191c90ee2f85e3c1571ed4a82179
Maisie Campbell <maisie-campbell@hotmail.com>
2017-06-26 13:10:43 +0100

    Changed config variables

--- health_overview.py
+++ health_overview.py
@@ -7,14 +7,9 @@ import os
 app = Flask(__name__)
 
 
-# MONGODB_HOST = 'localhost'
-# MONGODB_PORT = 27017
-# DBS_NAME = 'fitbit'
-# COLLECTION_NAME = 'activity'
-
-MONGO_URI = os.getenv('MONGO_URI', 'mongodb://root:health_overview1@ds131492.mlab.com:31492/heroku_0dl950d9')
-DBS_NAME = os.getenv('DBS_NAME', 'heroku_0dl950d9')
-COLLECTION_NAME = 'fitbit'
+MONGO_DB_URI = os.getenv('MONGO_DB_URI', 'mongodb://localhost:27017')
+MONGO_DB_NAME = os.getenv('MONGO_DB_NAME', 'fitbit')
+MONGO_DB_COLLECTION = os.getenv('MONGO_DB_COLLECTION', 'activity')
 
 
 
@@ -52,9 +47,9 @@ def health_overview():
     # Open a connection to MongoDB using a with statement such that the
     # connection will be closed as soon as we exit the with statement
     # The MONGO_URI connection is required when hosted using a remote mongo db.
-    with MongoClient(MONGO_URI) as conn:
+    with MongoClient(MONGO_DB_URI) as conn:
         # Define which collection we wish to access
-        collection = conn[DBS_NAME][COLLECTION_NAME]
+        collection = conn[MONGO_DB_NAME][MONGO_DB_COLLECTION]
         # Retrieve a result set only with the fields defined in FIELDS
         # and limit the the results to a lower limit of 20000
         projects = collection.find(projection=FIELDS, limit=20000)
