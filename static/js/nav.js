504e249261355e4497fbbbc70a5dcef5f695ac03
Maisie Campbell <maisie-campbell@hotmail.com>
2017-09-27 13:28:12 +0100

    amended toggle menu

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,9 +1,30 @@
-$("#navigation").addClass("js").before('<div id="menu">&#9776;</div>');
-	$("#menu").click(function(){
-		$("#navigation").toggle();
-	});
-	$(window).resize(function(){
-		if(window.innerWidth > 768) {
-			$("#navigation").removeAttr("style");
-		}
-	});
+// $("#navigation").addClass("js").before('<div id="menu">&#9776;</div>');
+// 	$("#menu").click(function(){
+// 		$("#navigation").toggle();
+// 	});
+// 	$(window).resize(function(){
+// 		if(window.innerWidth > 768) {
+// 			$("#navigation").removeAttr("style");
+// 		}
+// 	});
+
+$(document).ready(function(){
+  $("#nav-toggle a").click(function(event){
+    $("#main-nav").toggleClass("active");
+    $("#nav-toggle a").toggleClass("active");
+    event.preventDefault();
+  });
+}); 
+
+jQuery(function($) {
+    $(window).scroll(function() {
+        var scrollPos = $(window).scrollTop();
+        var navOpacity = scrollPos / 100;
+
+        $('.navbar').css("opacity", navOpacity);
+
+        if ($('nav').css('opacity') < 1) {
+            $('.navbar').css('opacity', 1);
+        }
+    });
+});
